602f9d72c1f093552262c6b0321f50500201ff71
Hindax <haar162@more2learn.de>
2022-11-28 10:04:06 +0100

    Overriding

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,11 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        Child child = new Child();
+        output(child.sayHello());
+        output(child.sayHello("Hello from "));
+
     }
 
     public static void output(String outputStr) {
