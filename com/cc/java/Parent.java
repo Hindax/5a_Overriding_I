fef5ad5ad783843f1ff62bb3be355f2ad28540a0
Hindax <haar162@more2learn.de>
2022-11-28 10:03:56 +0100

    Parent Child Overriding

---
+++ com/cc/java/Parent.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class Parent {
+
+    public String sayHello() {
+        return "Hello from child!";
+    }
+
+    public String sayHello(String str) {
+        return str + "parent!";
+    }
+
+}
